c92023d57960e9086d08db6a9221144edad5e000
gabriel <gabriel.cordova@tecsup.edu.pe>
2022-12-15 12:27:59 -0500

    primero commit

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./aboutSection.css";
+import {  Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/da1bubhns/image/upload/v1671053031/DALL_E_2022-12-14_16.17.50_q3qy0g.png"
+              alt="Founder"
+            />
+            <Typography>Gabriel Cordova Begazo,
+                        Luis Rivera Gonzales,
+                        Guido lucana Lopez
+            </Typography>
+            <span>
+              Programadores de 3DVision
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com/meabhisingh" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
